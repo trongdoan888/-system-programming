01eea7c58869aa30066d63e780e70399c94c651c
Tên của bạn <trongdoan7689@gmail.com>
2026-04-14 09:36:20 +0700

    .

---
+++ Stream/s7.py
@@ -0,0 +1,49 @@
+import os 
+
+
+FILE_NAME = "s7.txt"
+
+def tao_file_du_lieu(s7:list):
+
+    try:
+        with open(FILE_NAME, 'w') as f:
+            for phan_tu in s7:
+                f.write(f"{phan_tu}\n")
+        
+        print("Tạo file thành công......")
+    except Exception as e:
+        print(f"Lỗi: {e}")
+
+def tim_max_min():
+
+    if not os.path.exists(FILE_NAME):
+        print("File không tồn tại...")
+   
+    try:
+        with open(FILE_NAME,'r',encoding='utf-8') as f:
+            data = f.readlines()
+
+        list_data = []
+        for i in data:
+            list_data.append(i)
+        
+        list_data.sort()
+        max = list_data[len(list_data) - 1]
+        min = list_data[0]
+        count_min = list_data.count(min)
+        count_max = list_data.count(max)
+        
+
+        print(f"Gía trị min: {min} | Với số lần xuất hiện: {count_min}")
+        print(f"Gía trị max: {max} | Với số lần xuát hiện: {count_max}") 
+    except Exception as e:
+        print(f"Lỗi : {e}")
+
+if __name__ == "__main__":
+    
+    s7 = input("Nhập giá trị cách nhau bởi dấu cách: ")
+    s7 = s7.split()
+
+    tao_file_du_lieu(s7)
+    tim_max_min()
+
